d99a6c104874aa562bb694ec1269bb6986d440c1
Tobias Reithmaier <tobias.0104.reithmaier@gmail.com>
2024-03-10 14:23:53 +0100

    Update Text Preprocessing and Split into single steps

--- Pipeline/components/feature_text_preprocessing/feature_text_preprocessing.py
+++ Pipeline/components/feature_text_preprocessing/feature_text_preprocessing.py
@@ -12,6 +12,7 @@ import nltk
 import re
 from nltk.corpus import stopwords
 from nltk.stem import 	WordNetLemmatizer
+from nltk.corpus import wordnet
 
 
 parser=argparse.ArgumentParser("prep")
@@ -44,46 +45,79 @@ df=pd.concat(file_list)
 
 # Text preprocessing steps
 
-# Compile patterns for performance
+# Map NLTK POS-TAG to WordNet POS-TAG Format
+def map_pos_tag_nltk_to_wordnet(nltk_tag):
+    if nltk_tag.startswith('J'):
+        return wordnet.ADJ
+    elif nltk_tag.startswith('V'):
+        return wordnet.VERB
+    elif nltk_tag.startswith('N'):
+        return wordnet.NOUN
+    elif nltk_tag.startswith('R'):
+        return wordnet.ADV
+    else:          
+        # Return noun by default because this is the default value for the lemmatize call in the WordNet Lemmatizer
+        return wordnet.NOUN 
+
+# Compile patterns for data cleaning
 TAG_RE = re.compile(r'<[^>]+>')
 PUNCT_NUM_RE = re.compile('[^a-zA-Z]')
-SINGLE_CHAR_RE = re.compile(r"\s+[a-zA-Z]\s+")
+SINGLE_CHAR_RE = re.compile(r"(?:\s|^)[a-zA-Z](?=\s|$)")
 MULTI_SPACE_RE = re.compile(r'\s+')
 
 # Initialize objects outside of function
-lemmatizer = WordNetLemmatizer()
 stop_words = set(stopwords.words('english'))
-important_tags = ['FW', 'JJ', 'JJR', 'JJS', 'NN', 'NNP', 'NNS', \
-                      'RB', 'RBR', 'RBS', 'RP', 'UH',  \
-                      'VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ']
+
+important_tags = [
+    'FW', # foreign words
+    'JJ', 'JJR', 'JJS', # adjectives
+    'NN', 'NNP', 'NNS', 'NNPS', # nouns
+    'RB', 'RBR', 'RBS', # adverbs
+    'RP', # particles
+    'UH', # interjections
+    'VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ' # verbs
+]
+
+lemmatizer = WordNetLemmatizer()
 
 # Text preprocessing
-def preprocess_text(sen):
+def preprocess_text(sentence):
     # Lowercasing
-    sentence = sen.lower()
-    # Remove html tags
+    sentence = sentence.lower()
+    # Remove HTML tags
     sentence = TAG_RE.sub('', sentence)
     # Remove punctuations and numbers
     sentence = PUNCT_NUM_RE.sub(' ', sentence)
     # Single character removal
-    sentence = SINGLE_CHAR_RE.sub(' ', sentence)
+    sentence = SINGLE_CHAR_RE.sub('', sentence)
     # Remove multiple spaces
     sentence = MULTI_SPACE_RE.sub(' ', sentence).strip()
 
-    # Tokenize
+    # Tokenization
     tokens = nltk.word_tokenize(sentence)
-    # POS Tagging for all tokens in one go
-    tagged_tokens = nltk.pos_tag(tokens)
 
-    # Lemmatize and remove stopwords and non-important POS tags
-    filtered_tokens = [
-        lemmatizer.lemmatize(word) 
-        for word, tag in tagged_tokens
-        if word not in stop_words
-        and tag in important_tags
+    # Remove stopwords
+    tokens_no_stopwords = [word for word in tokens if word not in stop_words]
+
+    # POS Tagging for all remaining tokens in one go
+    tokens_pos_tagged = nltk.pos_tag(tokens_no_stopwords)
+
+    # Filter by POS tags
+    tokens_pos_filtered = [
+        (word, pos_tag)
+        for word, pos_tag in tokens_pos_tagged
+        if pos_tag in important_tags
     ]
 
-    processed_sentence = ' '.join(filtered_tokens)
+    # Lemmatization
+    tokens_lemmatized = [
+        lemmatizer.lemmatize(
+            word, pos=map_pos_tag_nltk_to_wordnet(pos_tag)
+        )
+        for word, pos_tag in tokens_pos_filtered
+    ]
+
+    processed_sentence = ' '.join(tokens_lemmatized)
     return processed_sentence
 
 # Now apply preprocess_text to all rows in the 'Text' column
